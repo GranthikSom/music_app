92d849e77db2ef3d3feac9fcf9cb9c70111bb0e5
Granthik Som <granthiksom@gmail.com>
2025-11-06 19:38:50 +0530

    what the fuck is going on

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 50),
+            Text("Welcome Back"),
+          ],
+        ),
+      ),
+    );
+  }
+}
